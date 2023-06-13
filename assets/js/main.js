bb1544fe9d5c8c2d46d9c6b6a999259ce5e77e2b
AnandaBintang <anandabintang4@gmail.com>
2023-06-13 16:17:48 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  $("#preloader").css({
+    opacity: "0",
+    transition: "all 1s ease",
+  });
+  setTimeout(() => {
+    $("#preloader").remove();
+  }, 500);
+
+  const mediaQuery = window.matchMedia("(min-width: 992px)");
+
+  if (mediaQuery.matches) {
+    // About Parallax
+    var b = document.getElementsByTagName("body")[0];
+    b.addEventListener("mousemove", function (event) {
+      parallaxed(event);
+    });
+    function parallaxed(e) {
+      var amountMovedX = (e.clientX * -0.4) / 10;
+      var amountMovedY = (e.clientY * -0.4) / 10;
+      var x = document.getElementsByClassName("about-indekos");
+      var i;
+      for (i = 0; i < x.length; i++) {
+        $(".img-about-1").css(
+          "transform",
+          `translate(${amountMovedX * -0.3}px, ${amountMovedY * -0.3}px)`
+        );
+        $(".img-about-2").css(
+          "transform",
+          `translate(${amountMovedX * -0.4}px, ${amountMovedY * -0.4}px)`
+        );
+        $(".img-about-3").css(
+          "transform",
+          `translate(${amountMovedX * -0.7}px, ${amountMovedY * -0.7}px)`
+        );
+        $(".img-about-4").css(
+          "transform",
+          `translate(${amountMovedX * -0.9}px, ${amountMovedY * -0.9}px)`
+        );
+        $(".img-about-5").css(
+          "transform",
+          `translate(${amountMovedX * -1.1}px, ${amountMovedY * -1.1}px)`
+        );
+        $(".img-about-6").css(
+          "transform",
+          `translate(${amountMovedX * -1.2}px, ${amountMovedY * -1.2}px)`
+        );
+        $(".img-about-7").css(
+          "transform",
+          `translate(${amountMovedX * -0.3}px, ${amountMovedY * -0.3}px)`
+        );
+      }
+    }
+  } else {
+    $(".main-visual").removeAttr("data-tilt");
+  }
+});
